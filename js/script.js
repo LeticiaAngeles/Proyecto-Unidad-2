4cf3e6b875c56f880d0ad037940e2af5e3710eb7
LeticiaAngele-ctc <sofialeticiaangeles9@gmail.com>
2024-08-19 20:42:19 -0500

    Quinto commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const firstNameInput = document.getElementById('userName');
+    const firstNameInput = document.getElementById('firstName');
 
     // Guardar el nombre en localStorage cuando se modifique el valor
     if (firstNameInput) {
         firstNameInput.addEventListener('input', function() {
-            localStorage.setItem('userName', firstNameInput.value);
+            localStorage.setItem('firstName', firstNameInput.value);
         });
     }
 });
